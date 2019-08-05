7fdec346f48f11ae7ba9245d48d6d656b95823e7
Arnaudboy <r.boyer895@laposte.net>
2019-08-05 14:54:53 +0200

    oincsd

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  def welcome(first_name, last_name)
+  	@first_name = first_name
+  	@last_name = last_name
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
+
   get '/', to: 'project#index'
+
   get '/team/', to: 'project#team'
+
   get 'project/contact'
-  get 'project/welcome_user'
+  
+  get '/welcome_user/:id', to: 'project#welcome_user' do
+  	puts "jsefksjnn"
+  	erb :welcome_user, locals: {id: params['first_name'], first_name: Gossip.welcome(params['first_name']), last_name: Gossip.welcome(params['last_name'])}
+  end
+
   get 'project/gossip'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
